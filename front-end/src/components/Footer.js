3b04d7ecc33d095a75e35a84e1264061c982ec40
miguelsedo <152197280+miguelsedo@users.noreply.github.com>
2025-02-28 13:22:46 +0100

    Delete front-end directory

--- front-end/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-    return (
-        <footer className="header-footer">
-            <p className="header-footer-text">© 2024 UNIR Relatos de Papel.</p>
-        </footer>
-    );
-}
--- front-end/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Header = () => {
-    return (
-        <header className="header-footer">
-            <h1 className="header-footer-text">Relatos de Papel</h1>
-        </header>
-    );
-}
